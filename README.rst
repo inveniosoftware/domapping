fda6fd1d77da757446ae42ce473ae4aa002e2cb2
Jorik van Kemenade <jorikvankemenade@gmail.com>
2019-07-31 16:39:08 +0200

    global: license change to MIT License

--- README.rst
+++ README.rst
@@ -1,25 +1,10 @@
 ..
-    This file is part of DoMapping.
-    Copyright (C) 2015, 2016 CERN.
+    This file is part of Invenio.
+    Copyright (C) 2015-2019 CERN.
 
-    DoMapping is free software; you can redistribute it
-    and/or modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of the
-    License, or (at your option) any later version.
+    Invenio is free software; you can redistribute it and/or modify it
+    under the terms of the MIT License; see LICENSE file for more details.
 
-    DoMapping is distributed in the hope that it will be
-    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DoMapping; if not, write to the
-    Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-    MA 02111-1307, USA.
-
-    In applying this license, CERN does not
-    waive the privileges and immunities granted to it by virtue of its status
-    as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 ===========
  DoMapping
@@ -31,19 +16,13 @@
 .. image:: https://img.shields.io/coveralls/inveniosoftware/domapping.svg
         :target: https://coveralls.io/r/inveniosoftware/domapping
 
-.. image:: https://img.shields.io/github/tag/inveniosoftware/domapping.svg
-        :target: https://github.com/inveniosoftware/domapping/releases
-
-.. image:: https://img.shields.io/pypi/dm/domapping.svg
-        :target: https://pypi.python.org/pypi/domapping
-
-.. image:: https://img.shields.io/github/license/inveniosoftware/domapping.svg
-        :target: https://github.com/inveniosoftware/domapping/blob/master/LICENSE
+.. image:: https://img.shields.io/pypi/v/domapping.svg
+        :target: https://pypi.org/pypi/domapping
 
 
 DoMapping provides a library and a CLI generating Elasticsearch mappings from JSON Schemas.
 
 *This is an experimental developer preview release.*
 
-* Free software: GPLv2 license
+* Free software: MIT license
 * Documentation: https://domapping.readthedocs.io/
--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of DoMapping.
-# Copyright (C) 2015, 2016, 2017 CERN.
+# This file is part of Invenio.
+# Copyright (C) 2015-2019 CERN.
 #
-# DoMapping is free software; you can redistribute it
-# and/or modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# DoMapping is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with DoMapping; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-# MA 02111-1307, USA.
-#
-# In applying this license, CERN does not
-# waive the privileges and immunities granted to it by virtue of its status
-# as an Intergovernmental Organization or submit itself to any jurisdiction.
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
 """DoMapping generates Elasticsearch mappings from JSON Schemas."""
 
@@ -114,7 +98,7 @@ setup(
     description=__doc__,
     long_description=readme + '\n\n' + history,
     keywords='jsonschema, elasticsearch',
-    license='GPLv2',
+    license='MIT',
     author='CERN',
     author_email='info@inveniosoftware.org',
     url='https://github.com/inveniosoftware/domapping',
@@ -134,7 +118,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
@@ -145,7 +129,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
     ],
     cmdclass={'test': PyTest},
 )
